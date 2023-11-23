e9a6955079617b5d4922ff9d16e3c146e9f2d12f
Jshakhwat <99986085+Jshakhwat@users.noreply.github.com>
2023-11-23 22:28:29 +0600

    added a java file

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+        // document.write('hello world');
+        // alert('alert function');
+        {/* console.log('alart function'); */}
+
+// document.write("<p style='color:Red;'>this is a para."+name$+"</p>" +name$);
+
+// var name$="johnny";
+
+let firstNum = prompt('enter 1st number');
+let secondNum = prompt('enter 2nd number');
+// document.write(firstNum+secondNum);
+//let firstNum = perse('enter 1st number');
+//let secondNum = prompt('enter 2nd number');
+//document.write(Number(firstNum)+Number(secondNum));
+
+//document.write(Number(prompt('enter 2nd number')+(Number(prompt('enter 2nd number')))
+
+
+document.write()
+let firstname = 'johnny';
+let lastName = 'khan';
+firstname +" "+lastName
